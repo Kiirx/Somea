9c46369e77a04ea6ae04f693205f036e40d0fb38
Selkii <43090601+Kiirx@users.noreply.github.com>
2024-04-04 18:13:42 +0200

    ➕ Project Base

---
+++ Somea.Kernel/Kernel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace Somea.Kernel
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos kernel.");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
